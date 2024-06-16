1fa28dc27d1357450217e509989c8b312c0a1b2a
naresh-dev08 <147803432+naresh-dev08@users.noreply.github.com>
2024-06-16 15:13:41 +0530

    Bit extra changes using constant class for error msgs

---
+++ src/main/java/com/nt/constants/InsurancePolicyConstantMessages.java
@@ -0,0 +1,17 @@
+package com.nt.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsurancePolicyConstantMessages {
+
+    public static final String SAVE_SUCCESS = "save-success";
+    public static final String SAVE_FAILURE = "save-failure";
+    public static final String FIND_BY_ID_FAILURE = "find-by-id-failure";
+    public static final String UPDATE_SUCCESS = "update-success";
+    public static final String UPDATE_FAILURE = "update-failure";
+    public static final String DELETE_SUCCESS = "delete-success";
+    public static final String DELETE_FAILURE = "delete-failure";
+    public static final String STATUS_CHANGE_SUCCESS = "Insurance Policy status is changed";
+    public static final String STATUS_CHANGE_FAILURE = "Insurance Policy is not found for status change";
+}
